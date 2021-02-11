834880b2e1c44f9149ee3df796be0b050a473ec4
Jarek Jasiewicz <jarekj@pop-os.localdomain>
2021-02-11 11:54:50 +0100

    example data fixes and documentation fixes

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import os
 os.chdir("/home/jarekj/Dropbox/CODE/salbec")
 from soilalbedo import soil, soilCurve
-from diurnalalbedo import albedo # nazwa do zmiany
+from diurnalalbedo import albedo 
 from diurnalalbedo import batch_albedo_main_times
 from soildatabase import soilDatabase
 import matplotlib.pyplot as plt
@@ -10,46 +10,21 @@ import pandas as pd
 import datetime
 
 
-
 #%%
-
-xerosol = soil()
-xerosol.importFromFile("ASSETS/xerosol.xlsx")
-xerosol.params
-
-fig,ax = plt.subplots()
-xerosol.drawSpectrum(ax) #Draw tghe spectrum on selected ax, zmiana dodać gca()
-model = soilCurve() # zmiana nazwy na soilModel
-model.fit(xerosol.a45,1.99,39) #zmiana API podać obiekt soil w całości
-model.drawFitted() #Draw the fitted curve,
-params = model.get_curve_model()
-
-alb = albedo()
-location=(17.2,50.1)
-alb.load_parameters(params,location) # przekazać obiekt model w całości
-alb.set_date_by_day(220)
-alb.get_record(header=True) # zmiana nazwy, zmienić domyślnie header
-alb.plot_time_curve("fig.pdf")
-
-times = alb.times_DataFrame() # zmienić nazwę
-
-# dodać batch dla całości
-
-#%%
-
-xerosol = soil()
-xerosol.importFromFile("ASSETS/xerosol.xlsx")
-model = soilModel()
-model.fit(xerosol,1.99,39)
-alb = albedo()
-alb.load_parameters(model,location=(17.2,50.1),errors = [2,3,5])
-alb.set_date_by_day(220)
-record = alb.get_day_record(header=True)
-times = alb.get_diurnal_albedo()
+xerosol = soilSpectrum() # create soil object
+xerosol.importFromFile("ASSETS/testing.xlsx") # import soil spectrum
+model = soilModel() #create soil model object
+model.fit(xerosol,1.99,39) # fits model to the spectrum
+alb = albedo() n# create albedo object
+alb.load_parameters(model,location=(17.2,50.1),errors = [2,3,5]) # load parameters: model, location and errors
+alb.set_date_by_day(220) # set day of the analysis
+record = alb.get_day_record(header=True) # get record with results
+times = alb.get_diurnal_albedo() # get details
 
 # images:
 xerosol.drawSpectrum()
 model.drawFitted()
 alb.plot_time_curve("time_curve.pdf")
 
+#%% import full abledo
 
